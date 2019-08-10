de99944ae7ec36e08154818d2174f1111b7b12b7
Jeff Lewis <474547+halldorr@users.noreply.github.com>
2019-08-09 20:46:26 -0400

    Change placeholder values to variables, allow them to be passed in

--- rest/controller/db.php
+++ rest/controller/db.php
@@ -5,11 +5,11 @@ class DB
 	private static $writeDBConnection;
 	private static $readDBConnection;
 
-	public static function connectWriteDB()
+	public static function connectWriteDB($dbType, $host, $dbname, $username, $password)
 	{
 		if (self::$writeDBConnection === null)
 		{
-			self::$writeDBConnection = new PDO('mysql:host=localhost;dbname=DBNAME;charset=utf8', 'DBUSER', 'DBPASSWORD');
+			self::$writeDBConnection = new PDO("$dbType:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 			self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
@@ -18,11 +18,11 @@ class DB
 		return self::$writeDBConnection;
 	}
 
-	public static function connectReadDB()
+	public static function connectReadDB($dbType, $host, $dbname, $username, $password)
 	{
 		if (self::$readDBConnection === null)
 		{
-			self::$readDBConnection = new PDO('mysql:host=localhost;dbname=DBNAME;charset=utf8', 'DBUSER', 'DBPASSWORD');
+			self::$readDBConnection = new PDO("$dbType:host=$host;dbname=$dbname;charset=utf8", $username, $password);
 			self::$readDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			self::$readDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
